24ad197a6316b904a34ea3b140c8a66ddcb70aff
asc3rr <borysgnacinski@gmail.com>
2021-01-08 21:02:57 +0100

    added rendering from json response

---
+++ web/renderer.js
@@ -0,0 +1,26 @@
+function get_articles(){
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var json_obj = JSON.parse(this.responseText);
+
+            json_obj.forEach(article_data => {
+                var title = article_data.title;
+                var date = article_data.date;
+
+                var article_template = `
+                    <article>
+                        <span class="title">` + title + `</span>
+                        <span class="date">` + date + `</span>
+                    </article>
+                `;
+
+                document.getElementById("articles").innerHTML += article_template;
+            });
+        }
+    }
+
+    http.open("GET", "../api?field=posts", true);
+    http.send();
+}
