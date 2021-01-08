a56dc4d637ab43ae2212c1d0cc4d329a597bcac1
asc3rr <borysgnacinski@gmail.com>
2021-01-08 23:00:26 +0100

    added css

--- web/post.php
+++ web/post.php
@@ -8,8 +8,18 @@
     <title>sendbyte.pl</title>
 
     <script src="renderer.js"></script>
+    <link rel="stylesheet" href="css/post.css">
+    <link rel="stylesheet" href="css/all.css">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
 </head>
 <body onload="get_article(<?php echo $id; ?>)">
-    <div id="title"></div>
-    <div id="content"></div>
+    <div id="logo">
+        sendbyte.pl
+    </div>
+    <article id="main">
+        <div id="title"></div>
+        <div id="content"></div>
+    </article>
 </body>
