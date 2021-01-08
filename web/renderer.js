d973f54e7500c79de658a328c612348e6444877f
asc3rr <borysgnacinski@gmail.com>
2021-01-08 22:29:46 +0100

    added api requesting to get post by id

--- web/renderer.js
+++ web/renderer.js
@@ -1,16 +1,20 @@
 function get_articles(){
+    var host = window.location.hostname;
     var http = new XMLHttpRequest();
 
     http.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             var json_obj = JSON.parse(this.responseText);
 
+            console.log(json_obj);
+
             json_obj.forEach(article_data => {
+                var id = article_data.id;
                 var title = article_data.title;
 
                 var article_template = `
                     <article>
-                        <span class="title">` + title + `</span>
+                        <a href="post.php?id=`+id+`"><span class="title">` + title + `</span></a>
                     </article>
                 `;
 
@@ -19,6 +23,26 @@ function get_articles(){
         }
     }
 
-    http.open("GET", "../api?field=posts", true);
+    http.open("GET", "../api/?id=*", true);
     http.send();
+}
+
+function get_article(id){
+    var host = window.location.hostname;
+    var http = new XMLHttpRequest();
+
+    http.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            var json_obj = JSON.parse(this.responseText);
+
+            var title = json_obj.title;
+            var content = json_obj.content;
+
+            document.getElementById("title").innerHTML = title;
+            document.getElementById("content").innerHTML = content;
+        }
+    }
+
+    http.open("GET", "../api/?id=" + id, true);
+    http.send()
 }
